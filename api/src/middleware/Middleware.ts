93471c0bfd341a092c1dd188218e17a5ec05ecd0
Fernando Estevam <fernandoestevam23@gmail.com>
2020-08-15 23:32:42 -0300

    Configurando erros com Middlware

--- api/src/middleware/Middleware.ts
+++ api/src/middleware/Middleware.ts
@@ -8,22 +8,14 @@ interface IError {
 
 class Middleware {
   erro(error: IError, request: Request, response: Response, next: NextFunction) {
-    console.log(error);
-
-    response.status(error.status || 500).json({
-      erro: true,
-      error: error
-    });
+    response.status(error.status || 500)
+    response.json({ error: error.message })
   }
 
   pageNotFound(request: Request, response: Response, next: NextFunction) {
-    const error: IError = {
-      status: 400,
-      message: 'Page not found'
-    }
-
-    new Error(error);
-    // erro.status = 404;
+    const error:IError = new Error('Page not found');
+    error.status = 404;
+    
     next(error);
   }
 }
