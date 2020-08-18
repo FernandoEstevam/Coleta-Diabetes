db2184f7ecbee667448ebd6eec078baed1cff4de
Fernando Estevam <fernandoestevam23@gmail.com>
2020-08-18 15:31:51 -0300

    Page Home finalizado

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 // Image 
 import logo from '../../assets/image/logo.png';
 import enfermeiro from '../../assets/image/enfermeiro.png';
+import userIcon from '../../assets/image/icons/user.png';
+import lockIcon from '../../assets/image/icons/lock.png';
+import lacoIcon from '../../assets/image/icons/laco.png';
 
 // Styles
 import './style.css'
@@ -11,25 +14,44 @@ function Home() {
   return(
     <div id="page-landing">
       <div id="page-landing-content" className="container">
-        <div className="logo-container">
-          <img src={logo} alt="Logo Diabetes" />
-          <h2>Controle de coleta</h2>
-        </div>
-        <div className="img-medico-container">
-          <img src={enfermeiro} alt="Enfermeiro"/>
+        <div className="content-image">
+          <div className="logo-container">
+            <img src={logo} alt="Logo Diabetes" />
+            <h2>Controle de coleta</h2>
+          </div>
+          <div className="img-medico-container">
+            <img src={enfermeiro} alt="Enfermeiro"/>
+          </div>
         </div>
 
         <form>
-          <div className="block-input">
-            <label htmlFor="login">Login</label>
-            <input type="text" name="login" id="login" />
+          <div className="input-block">
+            <label htmlFor="login">
+              <img src={userIcon} alt="Login" />
+            </label>
+            <input 
+              type="text" 
+              name="login" 
+              id="login" 
+              placeholder="login"  
+            />
           </div>
 
-          <div className="block-input">
-            <label htmlFor="password">password</label>
-            <input type="password" name="password" id="password" />
+          <div className="input-block">
+            <label htmlFor="password">
+              <img src={lockIcon} alt="Password" />
+            </label>
+            <input 
+              type="password" 
+              name="password" 
+              id="password" 
+              placeholder="password"
+            />
           </div>
-
+          <button type="submit">
+            Entrar
+            <img src={lacoIcon} alt="Entrar laco"/>
+          </button>
         </form>
       </div>
     </div>
